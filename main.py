4f6adc9b9ef39792b3414f0118eccddb2101a028
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2023-12-14 15:16:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tweepy
 import requests
-consumer_key = ''
-consumer_secret = ''
-access_token = ""
-access_token_secret = ""
+from datetime import date
+consumer_key = os.environ['TWITTER_API_KEY']
+consumer_secret = os.environ['TWITTER_API_SECRET_KEY']
+access_token = os.environ['TWITTER_ACCESS_TOKEN']
+access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 client = tweepy.Client(consumer_key=consumer_key,
                     consumer_secret=consumer_secret,
                     access_token=access_token,
                     access_token_secret=access_token_secret)
-response = client.create_tweet(text='hello world')
+days = date(2024,4,5) - date.today()
+response = client.create_tweet(text=days)
