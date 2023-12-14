c3c33e8a628a659fba50b168f94ded0b2beb2dc5
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2023-12-14 15:49:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,4 @@ client = tweepy.Client(consumer_key=consumer_key,
                     access_token=access_token,
                     access_token_secret=access_token_secret)
 days = date(2024,4,5) - date.today()
-cd = dumps(days, default=str)
-response = client.create_tweet(text=cd)
+response = client.create_tweet(text=days.days)
